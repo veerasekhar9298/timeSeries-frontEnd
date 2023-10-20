afd343a1b479ba6efcf82343acda746459330d48
veera sekhar <muthakaniveerashekhar@gmail.com>
2023-10-20 11:59:35 +0530

    data is displaying in real time working good

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import io from 'socket.io-client'
+import {useState} from 'react'
+const socket = io('http://localhost:5443')
 
 function App() {
+
+
+    const [sensorData,setSensorData] = useState({})
+
+
+    socket.on('data',(msg)=>{
+      const updated = {...sensorData}
+            updated[msg.timestamp] = msg.data
+            setSensorData(updated)
+    })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Sensor Log </h1>
+        {
+          Object.keys(sensorData).length >0 ? (Object.keys(sensorData).map((ele,i)=>{
+
+            return <div key = {i}>
+                  <h4> Time : {new Date(ele*60000).toLocaleTimeString()} </h4>
+
+                  {
+                    sensorData[ele].map((ele2,i2)=>{
+                      return <p> {ele2.name} ----- {ele2.origin} ---- {ele2.destination}</p>
+                    })
+                  }
+              </div>
+
+          })):(<h4> waiting for the Data.....</h4>)
+        }
     </div>
   );
 }
